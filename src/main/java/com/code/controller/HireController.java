876eb4b0eebb23acacb338c903771b3a73939172
kje <kje0740@gmail.com>
2024-06-17 09:12:32 +0900

    0617

--- src/main/java/com/code/controller/HireController.java
+++ src/main/java/com/code/controller/HireController.java
@@ -3,14 +3,17 @@ package com.code.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.code.dto.HireDto;
 import com.code.service.CompanyService;
+import com.code.service.HireService;
 
 @Controller
 public class HireController {
@@ -18,10 +21,21 @@ public class HireController {
 	@Autowired
 	CompanyService com_service;
 	
-	@GetMapping("/hire/insert")
-	public String hireInsert(@ModelAttribute("hdto") HireDto dto) {
+	@Autowired
+	HireService hservice;
+	
+	@GetMapping("/hire/main")
+	public String hireMain(@ModelAttribute("hdto") HireDto dto) {
 		
-		return null;
+		return "hire/hiremain";
+	}
+	
+	@PostMapping("/hire/insert")
+	public String hireInsert(@ModelAttribute("hdto") HireDto hdto) {
+		
+		hservice.hireInsert(hdto);
+
+		return "/hire/hirewrite";
 	}
 	
 	
