5e7be73ea78e3ab7df36167b19996d5916db87fc
byungjunnn <leebj4690@naver.com>
2024-06-25 16:52:58 +0900

    0625 1652

--- src/main/java/com/code/controller/AllSearchController.java
+++ src/main/java/com/code/controller/AllSearchController.java
@@ -2,9 +2,13 @@ package com.code.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.code.dto.CompanyIntroDto;
@@ -16,6 +20,12 @@ public class AllSearchController {
 	
 	@Autowired
 	AllSearchService aservice;
+	
+	@GetMapping("/allsearch/main")
+	public String allsearchmain(Model model, HttpSession session) {
+
+		return "allsearch/allsearchmain";
+	}
 	
 	@ResponseBody
 	@GetMapping("/allsearch/hire")
