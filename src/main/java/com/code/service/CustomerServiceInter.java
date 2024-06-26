e504ffe1b4980d7d076cdf81ba8829b75f8a4abb
byeonhyoyoung <gydud0109@naver.com>
2024-06-26 23:57:57 +0900

    community 완료

--- src/main/java/com/code/service/CustomerServiceInter.java
+++ src/main/java/com/code/service/CustomerServiceInter.java
@@ -2,14 +2,31 @@ package com.code.service;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.code.dto.CustomerDto;
 
 public interface CustomerServiceInter {
 
 	public int getTotalCount(); //전체 게시글 수 조회
 	public void insertCustomer(CustomerDto dto); //글 삽입
+	
+	//페이징 데이터 조회
+	//List<CustomerDto> getPagedDatas(@Param("startRow") int startRow, @Param("pageSize") int pageSize); 
+	
     List<CustomerDto> getAllDatas(); //모든 데이터 조회
     public CustomerDto getData(int cus_num); //특정 게시글 데이터 조회
     public void updateCustomer(CustomerDto dto);
     public void deleteCustomer(String cus_num);
+    
+    List<CustomerDto> getDatasByType(@Param("cus_top_type") String cus_top_type); //타입별 데이터 조회
+    int getCountByType(@Param("cus_top_type") String cus_top_type); //타입별 게시글 수 조회
+    List<CustomerDto> getPagedDatasByType(@Param("cus_top_type") String cus_top_type, @Param("startRow") int startRow, @Param("pageSize") int pageSize); //페이징 처리된 데이터 조회
+    
+    
+    /*
+    List<CustomerDto> getDatasByType(String cus_top_type); //타입별 데이터 조회
+    int getCountByType(String cus_top_type); //타입별 게시글 수 조회
+    List<CustomerDto> getPagedDatasByType(@Param("cus_top_type") String cus_top_type, @Param("startRow") int startRow, @Param("pageSize") int pageSize); //페이징 처리된 데이터 조회
+    */
 }
